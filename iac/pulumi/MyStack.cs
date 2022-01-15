3b0bfca2024ab487d82aabbcb713bbfd5a67d36f
Jonathas Costa <jonathas.costa@gmail.com>
2022-01-15 02:06:15 +0000

    alfa v1

--- iac/pulumi/MyStack.cs
+++ iac/pulumi/MyStack.cs
@@ -1,19 +1,31 @@
 using System;
+using System.Text;
 using System.Threading.Tasks;
 using Pulumi;
 using Pulumi.AzureNative.Resources;
 using Pulumi.AzureNative.Storage;
 using Pulumi.AzureNative.Storage.Inputs;
+using Pulumi.AzureNative.Web;
 
 class MyStack : Stack
 {
     public MyStack()
     {
-        var prNumber = Environment.GetEnvironmentVariable("RUN_NUMBER") ?? "0";
-        //Pulumi.AzureNative.LabServices.Environment.
-        // Create an Azure Resource Group
-        var resourceGroup = new ResourceGroup($"rg-{prNumber.PadLeft(3, '0')}");
+        var appName = Environment.GetEnvironmentVariable("APP_NAME") ?? throw new Exception("Missing 'APP_NAME' environment variable.");
+        var environment = Environment.GetEnvironmentVariable("ENVIRONMENT") ?? throw new Exception("Missing 'ENVIRONMENT' environment variable.");
+        var runNumber = Environment.GetEnvironmentVariable("RUN_NUMBER")?.PadLeft(3, '0') ?? throw new Exception("Missing 'RUN_NUMBER' environment variable.");
 
+        var resourceGroupName = new ResourceGroupNameBuilder()
+            .AppName(appName)
+            .Environment(environment)
+            .BuildNumber(runNumber)
+            .Build();
+
+        var resourceGroup = new ResourceGroup(resourceGroupName);
+        var staticApp = new StaticSite("", new StaticSiteArgs
+        {
+            ResourceGroupName = resourceGroupName,
+        });
         // Create an Azure resource (Storage Account)
         var storageAccount = new StorageAccount("sa", new StorageAccountArgs
         {
@@ -26,8 +38,7 @@ class MyStack : Stack
         });
 
         // Export the primary key of the Storage Account
-        this.PrimaryStorageKey = Output.Tuple(resourceGroup.Name, storageAccount.Name).Apply(names =>
-            Output.CreateSecret(GetStorageAccountPrimaryKey(names.Item1, names.Item2)));
+        this.PrimaryStorageKey = Output.CreateSecret(GetStorageAccountPrimaryKey(resourceGroupName, storageAccount.Name.ToString()));
     }
 
     [Output]
@@ -42,4 +53,58 @@ class MyStack : Stack
         });
         return accountKeys.Keys[0].Value;
     }
+}
+
+class ResourceGroupNameBuilder
+{
+    private string? _appName;
+    private string? _environment;
+    private string? _buildNumber;
+
+    public ResourceGroupNameBuilder AppName(string appName)
+    {
+        _appName = appName;
+        return this;
+    }
+
+    public ResourceGroupNameBuilder Environment(string environment)
+    {
+        _environment = environment;
+        return this;
+    }
+
+    public ResourceGroupNameBuilder BuildNumber(string buildNumber)
+    {
+        _buildNumber = buildNumber;
+        return this;
+    }
+
+    public string Build() => $"rg-{_appName}-{_environment}-{_buildNumber}";
+}
+
+class StaticSiteNameBuilder
+{
+    private string? _appName;
+    private string? _environment;
+    private string? _buildNumber;
+
+    public StaticSiteNameBuilder AppName(string appName)
+    {
+        _appName = appName;
+        return this;
+    }
+
+    public StaticSiteNameBuilder Environment(string environment)
+    {
+        _environment = environment;
+        return this;
+    }
+
+    public StaticSiteNameBuilder BuildNumber(string buildNumber)
+    {
+        _buildNumber = buildNumber;
+        return this;
+    }
+
+    public string Build() => $"stapp-{_appName}-{_environment}-{_buildNumber}";
 }
